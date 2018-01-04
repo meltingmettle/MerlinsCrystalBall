b590f437a1b84fe194eee69383930706b1a03b0a
Emmett Ling <emmettl@berkeley.edu>
2018-01-03 23:26:30 -0800

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,15 @@
+#In a game based on social deduction,  visual cues, deception, and unpredictability,  can a minions intent be revealed by careful objective analysis?
+#
+#In the long run, MCB will use TensorFlow to observe and predict behavioral patterns of spies using player metadata from past games.
+#Currently, this program uses a primitive system known as Suslevel™.  
+#As the program tracks plays, players will be given SusPoints™ for suspicious behavior which has been previously identified
+#
+
+def badApprove(player, Mission):
+  if player_approvedmission && missionOutcome == 0:
+      player.suslevel += 1
+      if player not in Mission.Team and Mission.count != 4:
+          player.suslevel +=1
+  
+
+#
