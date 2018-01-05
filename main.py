d5be42e8c4b8133eee2bc3febd4065f36f7a7dda
Emmett <emmettl@berkeley.edu>
2018-01-04 22:47:31 -0800

    Completed basic input functionality.

--- main.py
+++ main.py
@@ -315,12 +315,3 @@ play = Game()
 #Mission fails
 #votes
 #votes on mission passes/fails
-
-
-#Add plays
-
-
-# name = input("What's your name? ")
-# print("Nice to meet you " + name + "!")
-# age = input("Your age? ")
-# print("So, you are already " + str(age) + " years old, " + name + "!")
