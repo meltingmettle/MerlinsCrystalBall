f8b74f6880cc51a99099f94b743cc0a2edc414d7
Emmett Ling <emmettl@berkeley.edu>
2018-06-26 09:23:57 -0700

    Create interplayer_analysis.py

---
+++ interplayer_analysis.py
@@ -0,0 +1,9 @@
+#Use graph setup
+
+
+#Making team with or without people
+#Approving team
+#Rejecting team
+#similar votes
+
+#dynamic decisions?  If p6 is good, p7 is bad.  If p6 is bad, p3, p4, and p7  are good. 
