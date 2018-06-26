7bcabc941c7ac2e76767a20027659f3882d8eab4
Emmett Ling <emmettl@berkeley.edu>
2018-06-26 09:37:31 -0700

    Create machine_learning_brainstorm.py

---
+++ machine_learning_brainstorm.py
@@ -0,0 +1,21 @@
+"""
+For now, instead of actively solving the game, figure out how to discern patterns between profiles.
+Consider:
+  
+   
+
+
+#NOTE: Need to make Mission profiles and Player profiles.  
+
+
+Analyze past games with full data: how spies behaved and how players behaved.
+Do the same analysis without knowing whether the teams are good or bad.  
+Graph out the results and check for a score difference or other pattern.
+
+
+Player-centric: analyze the player's votes solely based on player actions.  (Probably need the mission consideration as well)
+Do a least-squares regression between the current player, past spies, and past knights.
+
+Mission-centric:  Analyze who approved the team, the memebers, and the outcome.  
+
+"""
